2004da1f74402a641f4167451f8acb210e040b3d
AthlonsDev <47662221+AthlonsDev@users.noreply.github.com>
2022-05-29 18:03:11 +0100

    Added fuel to co2 calculator checkboxes for type of fuel

--- app/src/main/java/com/example/green_app_project_01/Co2CheckActivity.kt
+++ app/src/main/java/com/example/green_app_project_01/Co2CheckActivity.kt
@@ -2,11 +2,14 @@ package com.example.green_app_project_01
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Debug
 import android.os.PersistableBundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.co2_check_layout.*
+import kotlin.math.roundToInt
 
 class Co2CheckActivity: AppCompatActivity() {
 
@@ -16,7 +19,21 @@ class Co2CheckActivity: AppCompatActivity() {
         setContentView(R.layout.co2_check_layout)
 
         button_co2.setOnClickListener {
+
             calculateCo2()
+        }
+
+        checkBox_petrol.setOnClickListener {
+            checkBox_diesel.isChecked = false
+            checkBox_lpg.isChecked = false
+        }
+        checkBox_diesel.setOnClickListener {
+            checkBox_petrol.isChecked = false
+            checkBox_lpg.isChecked = false
+        }
+        checkBox_lpg.setOnClickListener {
+            checkBox_diesel.isChecked = false
+            checkBox_petrol.isChecked = false
         }
 
     }
@@ -29,16 +46,58 @@ class Co2CheckActivity: AppCompatActivity() {
         return super.dispatchTouchEvent(ev)
     }
 
-    private fun calculateCo2() {
-
-//        Rates are for UK
+    private fun calcKilowatts(): Int {
+        //        Rates are for UK
 //        1kw/h = 0.281 co2
         val ukRates = 0.281
         val kwh = editText_kwh.text.toString().toInt()
 
-        val co2Ton = kwh * ukRates
+        val co2Ton = (kwh * ukRates).roundToInt()
+        return co2Ton
+    }
 
-        textView_co2_score.text = "You consume an avarage of ${co2Ton.toString()} Tonnes of Co2 every day"
+    private fun calcFuel(): Int {
+//        From Petrol
+        val fuelPerLitre = 652+1740
+
+        val inputLitres = fuelCo2_edit.text.toString().toInt()
+        val result = fuelPerLitre*inputLitres
+        Log.d("fuel", "fuel to co2 = $result")
+        return fuelPerLitre * inputLitres
+    }
+
+    private fun calculateCo2() {
+        val oilRates = 0.650
+        val coalRates = 0.9
+        val nucRates = 0.005
+        val windRates = 0.004
+        val solRates = 0.005
+
+        val kwh = editText_kwh.text.toString().toInt()
+
+        val co2Ton = (kwh * oilRates).roundToInt()
+
+        var fuelPerLitre = 652+1740
+        if (checkBox_petrol.isChecked) {
+            fuelPerLitre = 652+1740
+
+        }
+        else if (checkBox_diesel.isChecked) {
+            fuelPerLitre = 720+1920
+
+        }
+        else if (checkBox_lpg.isChecked) {
+            fuelPerLitre = 454+1211
+
+        }
+
+        val inputLitres = fuelCo2_edit.text.toString().toInt()
+        val fuel = fuelPerLitre*inputLitres
+
+
+        val result = co2Ton + fuel
+//        textView_co2_score.text = "You consume an avarage of ${kw.toString()} Tonnes of Co2 every day"
+        textView_co2_score.text = "You consume an avarage of ${result.toString()} Tonnes of Co2 every day"
     }
 
 }
